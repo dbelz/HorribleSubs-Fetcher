0559ac324b8f76004b47cddba0f92df514565d17
Daniel Belz <Daniel.Belz@EXXETA.com>
2019-03-13 13:29:26 +0100

    Removed old folders, added FetchBots sample

---
+++ samples/FetchBots/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HorribleSubsFetcher.Samples.FetchBots
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var fetcher = new Fetcher();
+            var tokenSource = new CancellationTokenSource();
+
+            var bots = await fetcher.FetchBotsAsync(tokenSource.Token);
+
+            foreach (var bot in bots)
+                Console.WriteLine(bot);
+
+            Console.ReadLine();
+        }
+    }
+}
