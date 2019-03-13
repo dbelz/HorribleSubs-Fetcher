00d8f16950cdc22ee3db6b8dc2d64b8b7648a4a4
Daniel Belz <Daniel.Belz@EXXETA.com>
2019-03-13 14:22:50 +0100

    Added FetchPackList sample

---
+++ samples/FetchPackList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HorribleSubsFetcher.Samples.FetchPackList
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var fetcher = new Fetcher();
+            var tokenSource = new CancellationTokenSource();
+
+            const string bot = "Ginpachi-Sensei";
+
+            var packList = await fetcher.FetchPackListAsync(bot, tokenSource.Token);
+
+            foreach (var item in packList)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
