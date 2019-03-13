ca2f4a2b1482d92b95e3256ef581a62f615ad31b
Daniel Belz <Daniel.Belz@EXXETA.com>
2019-03-13 14:31:56 +0100

    Added FindPacks sample

---
+++ samples/FindPacks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HorribleSubsFetcher.Samples.FindPacks
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            var fetcher = new Fetcher();
+            var tokenSource = new CancellationTokenSource();
+
+            var packList = await fetcher.FindPacksAsync("Toradora", tokenSource.Token);
+
+            foreach (var item in packList)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
