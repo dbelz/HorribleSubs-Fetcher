fe81f8d62d5e180a4cc8c00e5e353846b8699dac
Daniel Belz <Daniel.Belz@EXXETA.com>
2019-03-13 14:19:39 +0100

    Added FetchBotPackList sample

---
+++ samples/FetchBotPackList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HorribleSubsFetcher.Samples.FetchBotPackList
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var fetcher = new Fetcher();
+            var tokenSource = new CancellationTokenSource();
+
+            var packList = await fetcher.FetchPackListAsync(tokenSource.Token);
+
+            foreach (var item in packList)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
